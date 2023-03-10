24c7fd0478c83a298e76a884ab6f89c0b6466aac
melikegulerr <melikegulerr@gmail.com>
2023-03-10 22:01:36 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 "# loginpage" 
 "# loginpage" 
+"# loginpage" 
---
+++ mybutton.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class mybutton extends StatelessWidget {
+  final Function()? onTap;
+
+  const mybutton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(25),
+        margin: EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Center(
+          child: Text(
+            "Sign in",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
